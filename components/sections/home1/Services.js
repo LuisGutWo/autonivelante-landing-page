2ffbabf0b243815269bcda8a1a2867e5eadbd0e0
LuisGutWo <agutierrezwong@gmail.com>
2024-04-23 00:36:57 -0400

    feat services section

--- components/sections/home1/Services.js
+++ components/sections/home1/Services.js
@@ -1,56 +1,98 @@
 'use client'
 import Link from "next/link"
-import { useState } from "react"
-import { Image } from "react-bootstrap"
+import { Button, Col, Container, Image, Row } from "react-bootstrap"
 
 
 export default function Pricing() {
-    const [activeIndex, setActiveIndex] = useState(1)
-    const handleOnClick = (index) => {
-        setActiveIndex(index)
-    }
     return (
         <>
             <section id="services-section" className="service-section p_relative centred bg-color-1 sec-pad">
-                <div className="pattern-layer" style={{ backgroundImage: 'url(assets/images/shape/shape-12.png)' }}></div>
 
-                <div className="container">
-                    <section className="dark">
-                        <div className="container py-4">
-                            <article className="postcard dark blue">
-                                <a className="postcard__img_link" href="#">
-                                    <Image className="postcard__img" src="https://picsum.photos/1000/1000" alt="Image Title" />
-                                </a>
-                                <div className="postcard__text">
-                                    <h1 className="postcard__title blue"><a href="#">Podcast Title</a></h1>
-                                    <div className="postcard__subtitle small">
-                                        <time datetime="2020-05-25 12:00:00">
-                                            <i className="fas fa-calendar-alt mr-2"></i>Mon, May 25th 2020
-                                        </time>
-                                    </div>
-                                    <div className="postcard__bar"></div>
-                                    <div className="postcard__preview-txt">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, fugiat asperiores inventore beatae accusamus odit minima enim, commodi quia, doloribus eius! Ducimus nemo accusantium maiores velit corrupti tempora reiciendis molestiae repellat vero. Eveniet ipsam adipisci illo iusto quibusdam, sunt neque nulla unde ipsum dolores nobis enim quidem excepturi, illum quos!</div>
-                                    <ul className="postcard__tagbox">
-                                        <li className="tag__item"><i className="fas fa-tag mr-2"></i>Podcast</li>
-                                        <li className="tag__item"><i className="fas fa-clock mr-2"></i>55 mins.</li>
-                                        <li className="tag__item play blue">
-                                            <a href="#"><i className="fas fa-play mr-2"></i>Play Episode</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </article>
+                <div className="container py-4">
+                    <article className="postcard">
+                        <Link className="postcard__img_link" href="#">
+                            <div className="postcard__img" style={{ backgroundImage: "url(assets/images/services-banner-image.jpg)" }}></div>
+                        </Link>
+                        <div className="postcard__text">
+                            <h1 className="postcard__title blue text-light text-start"><Link href="#">somos Autonivelante
+                                <br />by Instafloor </Link></h1>
+                            <div className="postcard__bar"></div>
+                            <div className="postcard__preview-txt">Somos expertos en superficies en autonivelantes cementicios, autonivelacion, reparación de superficies, sobrelosas, afinado de lozas, retape y reparación de fisuras.
+                                <br />
+                                Especialistas en instalación y reparación en toda la gama de pisos vinilicos en rollos y en palmetas.
+                            </div>
 
+                            <Container fluid="md" className="mb-5 text-center">
+                                <Row>
+                                    <Col>
+                                        <div className="postcard__content">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                                            </svg> <p className="text-light">Autonivelantes Cementicios</p>
+                                        </div>
+                                    </Col>
+                                    <Col>
+                                        <div class="postcard__content">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                                            </svg> <p className="text-light">Autonivelacion</p>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <div class="postcard__content">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                                            </svg> <p className="text-light">Preparación de Superficies</p>
+                                        </div>
+                                    </Col>
+                                    <Col>
+                                        <div class="postcard__content">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                                            </svg> <p class="text-light">Sobrelosas</p>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <div class="postcard__content">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                                            </svg> <p class="text-light">Afinado de Losas</p>
+                                        </div>
+                                    </Col>
+                                    <Col>
+                                        <div class="postcard__content">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                                            </svg> <p class="text-light">Retape</p>
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <div class="postcard__content">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                                            </svg> <p class="text-light">Reparación de Fisuras</p>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Container>
+
+
+
+
+                            <Button variant="outline-light" style={{ width: "12rem", height: "4rem" }}>Contáctenos
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8" />
+                                </svg>
+                            </Button>{' '}
                         </div>
-                    </section>
-
-                    
-
-
-
-
-
+                    </article>
                 </div>
-
             </section>
         </>
     )
