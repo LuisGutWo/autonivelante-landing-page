1dfbbbbd645f7d5d0b2eee150aae5df558faef89
LuisGutWo <agutierrezwong@gmail.com>
2024-06-14 23:41:13 -0400

    feat banner: background video

--- components/sections/Banner.js
+++ components/sections/Banner.js
@@ -1,15 +1,27 @@
 'use client'
 import Link from "next/link"
+import React, { useEffect, useRef } from 'react';
 
 export default function Banner() {
+    const videoRef = useRef();
+
+    useEffect(() => {
+        setTimeout(() => {
+            videoRef.current.play();
+        }, 5000);
+    }, []);
+
     return (
         <>
             <section className="banner-style alternat-2 p_relative">
+                <video ref={videoRef} className="banner-video" controls autoPlay width="100%" height="100%" loop muted>
+                    <source src="https://firebasestorage.googleapis.com/v0/b/login-huellitas.appspot.com/o/autonivelante_bg_video.mp4?alt=media&token=82033e63-9eeb-4802-a5d3-2b9bdd19e5fb" type="video/mp4" />
+                </video>
                 <div className="banner__container">
                     <div className="container-item p_relative">
-                        <div className="content-box wow fadeIn animated" data-wow-delay="03ms" data-wow-duration="2000ms" >
-                            <h2>Somos expertos<br />en nivelar superficies<br />industriales y<br />residenciales</h2>
-                            <div className="btn-box">
+                        <div className="content-box" data-wow-delay="03ms" data-wow-duration="2000ms" >
+                            <h2 className="wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1000ms">Somos expertos<br />en nivelar superficies<br />industriales y<br />residenciales</h2>
+                            <div className="btn-box wow fadeInRight animated" data-wow-delay="00ms" data-wow-duration="2000ms">
                                 <Link href="#contact" className="theme-btn-one">Contáctenos
 
                                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
