f1796c7db2a136b2fcb770b6b0284f031fc2413e
LuisGutWo <agutierrezwong@gmail.com>
2024-06-21 05:04:37 -0400

    final fix deployed issues

--- app/works-gallery/page.js
+++ app/works-gallery/page.js
@@ -18,7 +18,7 @@ export default async function WorksGallery() {
                         <h1>Works Gallery</h1>
                         <Row className="justify-content-center">
 
-                            <Col key={index} xl={4} lg={4} md={6} sm
+                            <Col xl={4} lg={4} md={6} sm
                                 className="mb-4">
                                 <h2>Hola, soy la galería de trabajos</h2>
                             </Col>
