a5484f103d6158a9730c9eda7d9f9bbcab9f85f1
LuisGutWo <agutierrezwong@gmail.com>
2024-06-21 05:22:52 -0400

    new tray to... uhmmm i dont know

--- components/elements/Preloader.js
+++ components/elements/Preloader.js
@@ -1,9 +1,11 @@
 export default function Preloader() {
     return (
         <>
-            <div className="loader-wrap">
-                <div className="spinner-border" role="status">
-                    <span className="sr-only">Cargando...</span>
+            <div>
+                <div className="loader-wrap">
+                    <div className="spinner-border" role="status">
+                        <span className="sr-only">Cargando...</span>
+                    </div>
                 </div>
             </div>
 
