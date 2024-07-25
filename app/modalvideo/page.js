5bf9cb02ebd9abf4cb98bd3b486f86547cf0b226
LuisGutWo <agutierrezwong@gmail.com>
2024-07-25 05:37:04 -0400

    fix: video render issue

--- app/modalvideo/page.js
+++ app/modalvideo/page.js
@@ -11,26 +11,26 @@ const ModalVideoPage = () => {
   const [isClient, setIsClient] = useState(false);
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
-  if (setIsClient) {
-    console.log(setIsClient);
-  }
-
   useEffect(() => {
     setIsClient(true);
   }, []);
+
+  const imageSrc =
+    "https://firebasestorage.googleapis.com/v0/b/login-huellitas.appspot.com/o/autonivelante_contact_bg.png?alt=media&token=385298ec-ac43-43a5-94d8-52df6e8b50c8";
+
   return (
     <Layout headerStyle={4} footerStyle={1}>
       <section className="modalvideo__section">
         <div className="modalvideo__section-bg">
-          <Image
-            src={
-              "https://firebasestorage.googleapis.com/v0/b/login-huellitas.appspot.com/o/autonivelante_contact_bg.png?alt=media&token=385298ec-ac43-43a5-94d8-52df6e8b50c8"
-            }
-            layout="fill"
-            style={{ width: "100%", height: "100%" }}
-            alt="Background Autonivelante Image"
-            onError={(e) => console.error("Error loading image:", e)}
-          />
+          {imageSrc && (
+            <Image
+              src={imageSrc}
+              layout="fill"
+              style={{ width: "100%", height: "100%" }}
+              alt="Background Autonivelante Image"
+              onError={(e) => console.error("Error loading image:", e)}
+            />
+          )}
         </div>
         <div className="modalvideo__section-inner">
           {isClient && (
@@ -38,13 +38,16 @@ const ModalVideoPage = () => {
               url="https://youtu.be/tBoRtfl5Kfk?si=MC9GvXxcMg9sVwZ8"
               controls={false}
               width={isMobile ? "100%" : "60%"}
-              height={isMobile ? "40vh" : "80%"}
+              height={isMobile ? "40%" : "80%"}
               loop
               onError={(e) => console.error("Error loading video:", e)}
+              onReady={() => {
+                console.log("Player is ready");
+              }}
             />
           )}
           <div className="exit__button-card">
-            <Link href="#about-section">
+            <Link href="#about-section" title="Exit Button Link Icon">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="40"
@@ -58,6 +61,7 @@ const ModalVideoPage = () => {
                 className="bi bi-x-lg"
                 viewBox="0 0 16 16"
                 onClick={() => window.history.back()}
+                aria-label="Modal Video Exit Button"
                 role="button"
                 tabIndex={0}
               >
