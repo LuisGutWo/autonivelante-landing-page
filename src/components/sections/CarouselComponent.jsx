39721967d625468aaed17b860a8c1f99d67da030
LuisGutWo <agutierrezwong@gmail.com>
2024-08-06 01:30:19 -0400

    fix: carousel component importing module

--- src/components/sections/CarouselComponent.jsx
+++ src/components/sections/CarouselComponent.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import CarouselPage from "./CarouselPage";
-import { waitSeconds } from "@/src/utils/helpers";
 import { getData } from "@/src/lib/getData";
 export default async function CarouselComponent() {
   const products = (await getData(process.env.NEXT_STRAPI_URL)) ?? [];
-  await waitSeconds(1500);
 
   return (
     <div className="carousel__container">
