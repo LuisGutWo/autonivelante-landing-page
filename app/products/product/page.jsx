58c568d9acb7ac13ff80ee61de2d7cbc0b0dda8d
LuisGutWo <agutierrezwong@gmail.com>
2024-08-13 04:14:14 -0400

    fix: product page several issues4

--- app/products/product/page.jsx
+++ app/products/product/page.jsx
@@ -4,16 +4,27 @@ import Breadcrumb from "@/src/components/common/Breadcrumb/Breadcrumb";
 import { getSingleProduct } from "@/src/utils/helpers";
 import MainCardDetail from "@/app/products/product/MainCardDetail";
 import CarouselComponent from "@/src/components/sections/CarouselComponent";
+import { useRouter } from "next/router";
+
+export async function getStaticPaths() {
+  // Lógica para obtener todos los IDs de productos
+  const paths = [{ params: { product: "products/product" } }];
+  return { paths, fallback: false };
+}
+export async function getStaticProps({ params }) {
+  // Lógica para obtener los datos del producto basado en params.product
+  const productData = { id: params.product, name: "Producto Ejemplo" };
+  return { props: { product: productData } };
+}
 
 const NotFoundProduct = () => <div>No se encontró el producto</div>;
 
-export default async function SingleProduct({ searchParams }) {
-  const idString = searchParams?.id;
-  if (!idString) {
+export default async function SingleProduct({ products }) {
+  if (!products) {
     return <NotFoundProduct />;
   }
 
-  const id = Number(idString);
+  const id = Number(products);
 
   try {
     const product = await getSingleProduct(id);
